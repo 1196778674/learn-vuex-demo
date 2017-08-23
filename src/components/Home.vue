f5396a0c4e3f6f1055a3df523a4961c9408ca8a7
liufei <liufei@chupinxiu.com>
2017-08-23 17:36:50 +0800

    t

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		home
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'Home',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
