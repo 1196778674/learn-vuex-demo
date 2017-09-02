97199d4f77581c581bf02ad731a9d086b468eeb7
liufei <liufei@chupinxiu.com>
2017-09-02 10:26:53 +0800

    url

--- src/store/header.js
+++ src/store/header.js
@@ -2,7 +2,7 @@ export default {
 	state: {
 		title: '厨品秀',
 		user_name: '小灰灰',
-		nav_list: [{id: 1, name: '仓库', url: 'warehouse'},{id: 2, name: '物料', url: 'metaril'},{id: 3, name: '前台', url: 'qiantai'}],
+		nav_list: [{id: 1, name: '仓库', url: 'home'},{id: 2, name: '物料', url: 'metaril'},{id: 3, name: '前台', url: 'qiantai'}],
 		drop_list: [{id: 1, name: '个人中心'},{id: 2, name: '退出'}]
 	}
 }
